55221b2d13b916b433614d3cd4a3f75c0be3ce56
Matheus Odilon <matheusodilon0@gmail.com>
2022-10-10 12:40:02 -0300

    setting port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 8000!')
 })
