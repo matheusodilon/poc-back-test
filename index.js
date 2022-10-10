659694a48d48bbae999ee8bb4e056c74f2e2b861
Matheus Odilon <matheusodilon0@gmail.com>
2022-10-10 15:48:17 -0300

    cors

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 var app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get('/render', function (req, res) {
+app.get('/random', async function (req, res) {
   const randomNumber = Math.floor(Math.random() * 100);
-  res.send(`${randomNumber}`)
+  const data = randomNumber;
+  console.log('random', randomNumber);
+  res.json({ value: randomNumber })
 })
 
 app.post('/sum', (req, res) => {
   const { first_value, second_value } = req.body;
   const result = first_value + second_value;
+  console.log('sum result', result);
   res.send({
     data: { result }
   })
